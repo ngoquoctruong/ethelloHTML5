5f2caea49f33e551b0a5d7e3c8c74eb6a2fb6483
Ngo Quoc Truong <ngoquocturong302@gmail.com>
2020-12-11 15:14:23 +0700

    Updated

---
+++ src/settings.185e2.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/landing.fire",orientation:"",jsList:[],bundleVers:{internal:"279e0",main:"2f4a7"}};
